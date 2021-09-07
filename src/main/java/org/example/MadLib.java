f0ea049e1d462eb55074f34f39abaee4d9731bc9
Daniela <danigomezdugan@gmail.com>
2021-09-07 16:51:23 -0400

    updating file

--- src/main/java/org/example/MadLib.java
+++ src/main/java/org/example/MadLib.java
@@ -1,26 +1,23 @@
 package org.example;
-
 import java.util.Scanner;
 
 /**
- * Hello world!
- *
+ * UCF COP3330 Fall 2021 Assignment 1 Solution
+ *Copyright 2021 Daniela Gomez-Dugan
  */
 public class MadLib
 {
     public static void main( String[] args )
     {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a holiday: ");
-        String holiday = input.nextLine();
         System.out.print("Enter a noun: ");
-        String noun_1 = input.nextLine();
-        System.out.print("Enter a place: ");
-        String place = input.nextLine();
+        String noun = input.nextLine();
+        System.out.print("Enter a verb: ");
+        String verb = input.nextLine();
+        System.out.print("Enter a adverb: ");
+        String adverb = input.nextLine();
         System.out.print("Enter a adjective: ");
         String adjective = input.nextLine();
-        System.out.print("Enter a noun: ");
-        String noun_2 = input.nextLine();
-        System.out.println("\"I can't believe it's already " + holiday + "! I can't wait to put on my " + noun_1 + " and visit every " + place + " in my neighborhood. \nThis year, I am going dressed up as a(n) " + adjective + " " + noun_2 + "!\" Isn't that hilarious?");
+        System.out.println("\"Do you " + verb + " your " + adjective + " " + noun + " " + adverb + "?\" Isn't that hilarious?");
     }
 }
